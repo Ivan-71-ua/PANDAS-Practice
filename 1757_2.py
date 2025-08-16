6cccdfbbfb3c411d989b5c3adf38c05cb9139e9c
HROZNY17 <ivansymochko2006@gmail.com>
2025-08-16 18:44:04 +0300

    LeetCode Problem 1757 v2 PANDAS modified

--- 1757_2.py
+++ 1757_2.py
@@ -7,6 +7,7 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
     mask_low = products['low_fats'] == 'Y'
     mask_rec = products['recyclable'] == 'Y'
     both = mask_low & mask_rec
-    res = products[both][['product_id']]
-    res.reset_index(drop=True, inplace=True)
-    return res
+    #res = products[both][['product_id']]
+    #not really need
+    #res.reset_index(drop=True, inplace=True)
+    return products[both][['product_id']]
