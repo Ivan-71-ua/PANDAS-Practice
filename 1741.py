732a1b1fe001b4b5d280f800772cc26672f26f99
HROZNY17 <ivansymochko2006@gmail.com>
2025-08-19 13:04:04 +0300

    LeetCode Problem 1741 v1 PANDAS mod

--- 1741.py
+++ 1741.py
@@ -1,12 +1,14 @@
 
 import pandas as pd
 
+
+
 def total_time(employees: pd.DataFrame) -> pd.DataFrame:
     data = {}
     for _, row in employees.iterrows():
-        if (row['day'], row['emp_id']) not in data:
-            data[(row['day'], row['emp_id'])] = 0
-        data[(row['day'], row['emp_id'])] += row['out_time'] - row['in_time']
+        if (row['event_day'], row['emp_id']) not in data:
+            data[(row['event_day'], row['emp_id'])] = 0
+        data[(row['event_day'], row['emp_id'])] += row['out_time'] - row['in_time']
     rows = []
     for (day, emp_id), total in data.items():
         rows.append({'day' : day, 'emp_id' : emp_id, 'total_time' : total})
