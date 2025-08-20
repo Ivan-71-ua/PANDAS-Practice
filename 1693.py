a6f7aaa5d1d39ceebdff2086f1d3c1cf6a7d40c0
HROZNY17 <ivansymochko2006@gmail.com>
2025-08-20 11:19:04 +0300

    LeetCode Problem 1693 v1 PANDAS

---
+++ 1693.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    grouped = daily_sales.groupby(['date_id', 'make_name'])
+    counts = grouped.nunique(['lead_id', 'partner_id'])
+    res = counts.reset_index()
+    res.rename(columns = {'lead_id' : 'unique_leads', 'partner_id' : 'unique_partners'}, inplace = True)
+    return res
