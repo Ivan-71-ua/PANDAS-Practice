f3294f34c188bf6d4dc930876d1094e73426fd07
HROZNY17 <ivansymochko2006@gmail.com>
2025-09-12 10:49:57 +0300

    LeetCode Problem 1148 v1 PANDAS

---
+++ 1148.py
@@ -0,0 +1,6 @@
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    dp = views.loc[views.author_id == views.viewer_id][['author_id']].drop_duplicates().sort_values(by = "author_id")
+    return dp.rename(columns = {'author_id':'id'})
