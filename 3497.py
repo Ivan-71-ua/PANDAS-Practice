3fc9ab8a67bf88a5e77a91ceadbf0a733864cfe8
HROZNY17 <ivansymochko2006@gmail.com>
2025-10-16 10:59:26 +0300

    LeetCode Problem 3497 v2 PANDAS

--- 3497.py
+++ 3497.py
@@ -2,8 +2,24 @@
 import pandas as pd
 
 def analyze_subscription_conversion(user_activity: pd.DataFrame) -> pd.DataFrame:
-    grp = user_activity.groupby('user_id', as_index = False).agg(
-        trial_avg_duration = ('activity_duration', 'mean'),
-        paid_avg_duration = ('activity_duration', 'mean')
+    round2 = lambda x: round(x + 0.0001, 2)
+
+    trial = (
+        user_activity[user_activity['activity_type'] == 'free_trial']
+        .groupby('user_id', as_index = False)['activity_duration']
+        .mean()
+        .rename(columns={'activity_duration': 'trial_avg_duration'})
     )
-    return grp.loc[grp['paid_avg_duration'] > 0]
+
+    paid = (
+        user_activity[user_activity['activity_type'] == 'paid']
+        .groupby('user_id', as_index = False)['activity_duration']
+        .mean()
+        .rename(columns={'activity_duration': 'paid_avg_duration'})
+    )
+
+    mr = pd.merge(trial, paid, on = 'user_id')
+
+    mr['trial_avg_duration'] = mr['trial_avg_duration'].apply(round2)
+    mr['paid_avg_duration'] = mr['paid_avg_duration'].apply(round2)
+    return mr.sort_values('user_id')
