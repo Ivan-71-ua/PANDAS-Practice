5e1d4d8315e9c5f1980aed444df67e210f3d816b
HROZNY17 <ivansymochko2006@gmail.com>
2025-10-30 10:20:09 +0200

    LeetCode Problem 608 v1 PANDAS

---
+++ 608.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def tree_node(tree: pd.DataFrame) -> pd.DataFrame:
+    tree['type'] = 'Inner'
+    tree.loc[~tree['id'].isin(tree['p_id']), 'type'] = 'Leaf'
+    tree.loc[tree['p_id'].isnull(), 'type'] = 'Root'
+    return tree[['id', 'type']]
